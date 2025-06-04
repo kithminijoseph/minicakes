0dffeaf570a47328ec025f1d81206906b918a2eb
Kithmini Joseph <KithminiJoseph@ibm.com>
2025-06-04 13:19:11 +0100

    create base file structure

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const postsRoute = require('./routes/posts'); // Import the routes
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/posts', postsRoute); // Use the imported routes
+
+// MongoDB connection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database'))
+  .catch((error) => console.log(error));
+
+app.get('/', (req, res) => {
+  res.send('Hello, MERN!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
