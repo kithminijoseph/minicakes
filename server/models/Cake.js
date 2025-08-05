dd28392d7e54c51ed9772afd8580e040a2f6b565
Kithmini Joseph <kithminijoseph@gmail.com>
2025-08-05 23:01:38 +0100

    update

--- server/models/Cake.js
+++ server/models/Cake.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 
-const cakeSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  image: String
+const CakeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  images: [String],
+  description: { type: String },
+  price: { type: Number, required: true },
+  category: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('Cake', cakeSchema);
+const Cake = mongoose.model('Cake', CakeSchema);
+
+export default Cake;
