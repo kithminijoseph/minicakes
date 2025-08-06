c89ebf73463008c9bc9d268d24c91588c500a654
Kithmini Joseph <kithminijoseph@gmail.com>
2025-08-06 13:41:21 +0100

    redeploy

--- server/server.js
+++ server/server.js
@@ -14,8 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
   .catch(err => console.error(err));
+
 
 // Use routes
 app.use('/api/cakes', cakeRoutes);
